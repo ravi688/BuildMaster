927f4296fa9128fdbf22e7a487af06e6bda89a1a
ravi688 <rp0412204@gmail.com>
2025-01-18 05:21:28 +0530

    Implement project initialization

--- source/main.cpp
+++ source/main.cpp
@@ -2,13 +2,46 @@
 #include <cstdlib>
 #include <format>
 #include <iterator>
+#include <filesystem>
 
 #include <CLI/CLI.hpp>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
 
 //  build_master init
 static void intializeProject(std::string_view projectName, std::string_view canonicalName, bool isForce)
 {
 	std::cout << std::format("Project Name = \"{}\", Canonical Name = \"{}\"", projectName, canonicalName) << "\n";
+	if(!isForce && std::filesystem::exists("build_master.json"))
+	{
+		std::cout << "Error: build_master.json already exists, please remove it first or pass --force to overwrite it\n";
+		exit(EXIT_FAILURE);
+	}
+	json buildMasterJson = 
+	{
+		{ "header_dir", "include" },
+		{ "canonical_name", canonicalName },
+		{ "project_name" , projectName },
+		{ "targets",
+			{ canonicalName, 
+				{
+					{ "is_executable", true },
+					{ "sources", { "source/main.c" } }
+				}
+			}
+		}
+	};
+
+	std::ofstream file("build_master.json", std::ios_base::trunc);
+	if(!file.is_open())
+	{
+		std::cout << "Error: Failed to create build_master.json file\n";
+		exit(EXIT_FAILURE);
+	}
+	file << buildMasterJson.dump(4, ' ', true) << std::endl;
+	file.close();
+	std::cout << "Success: build_master.json is generated\n";
 }
 
 // build_master meson
@@ -44,6 +77,7 @@ int main(int argc, const char* argv[])
 		scInit->add_option("--name", projectName, "Name of the Project")->required();
 		scInit->add_option("--canonical_name", canonicalName, 
 			"Conanical name of the Project, typically used for naming files")->required();
+		scInit->add_flag("--force", isForce, "Forcefully overwrites the existing build_master.json file, use it carefully");
 		scInit->callback([&]() { intializeProject(projectName, canonicalName, isForce); });
 	}
 	
