c41031f51031c30cfd4c7aadcfc5b062bc016794
ravi688 <rp0412204@gmail.com>
2025-01-18 06:12:38 +0530

    Install meson.build.template and also define MESON_BUILD_TEMPLATE_PATH

--- source/main.cpp
+++ source/main.cpp
@@ -9,11 +9,58 @@
 
 using json = nlohmann::json;
 
+static constexpr std::string_view gBuildMasterJsonFilePath = "build_master.json";
+static constexpr std::string_view gMesonBuildScriptFilePath = "meson.build";
+
+static bool IsRegenerateMesonBuildScript()
+{
+	if(std::filesystem::exists(gMesonBuildScriptFilePath))
+	{
+		auto mesonBuildScriptTime = std::filesystem::last_write_time(gMesonBuildScriptFilePath);
+		auto buildMasterJsonTime = std::filesystem::last_write_time(gBuildMasterJsonFilePath);
+		return buildMasterJsonTime >= mesonBuildScriptTime;
+	}
+ 	return true;
+}
+
+static json ParseBuildMasterJson()
+{
+	std::ifstream stream(gBuildMasterJsonFilePath.data());
+	if(!stream.is_open())
+	{
+		std::cout << "Error: Failed to open " << gBuildMasterJsonFilePath << "\n";
+		exit(EXIT_FAILURE);
+	}
+	json data = json::parse(stream);
+	return data;
+}
+
+static void GenerateMesonBuildScript()
+{
+	std::cout << std::format("Generating {}", gMesonBuildScriptFilePath) << "\n";
+	json buildMasterJson = ParseBuildMasterJson();
+	std::string_view str = MESON_BUILD_TEMPLATE_PATH;
+	std::cout << "MESON_BUILD_TEMPLATE_PATH: " << str << "\n";
+}
+
+static void RegenerateMesonBuildScript()
+{
+	if(!std::filesystem::exists(gBuildMasterJsonFilePath))
+	{
+		std::cout << "Error: build_master.json doesn't exists, please execute the following:\n"
+					"build_master init --name <your project name> --canonical_name <filename friendly project name>\n";
+		exit(EXIT_FAILURE);
+	}
+
+	if(IsRegenerateMesonBuildScript())
+		GenerateMesonBuildScript();
+}
+
 //  build_master init
-static void intializeProject(std::string_view projectName, std::string_view canonicalName, bool isForce)
+static void IntializeProject(std::string_view projectName, std::string_view canonicalName, bool isForce)
 {
 	std::cout << std::format("Project Name = \"{}\", Canonical Name = \"{}\"", projectName, canonicalName) << "\n";
-	if(!isForce && std::filesystem::exists("build_master.json"))
+	if(!isForce && std::filesystem::exists(gBuildMasterJsonFilePath))
 	{
 		std::cout << "Error: build_master.json already exists, please remove it first or pass --force to overwrite it\n";
 		exit(EXIT_FAILURE);
@@ -33,7 +80,7 @@ static void intializeProject(std::string_view projectName, std::string_view cano
 		}
 	};
 
-	std::ofstream file("build_master.json", std::ios_base::trunc);
+	std::ofstream file(gBuildMasterJsonFilePath.data(), std::ios_base::trunc);
 	if(!file.is_open())
 	{
 		std::cout << "Error: Failed to create build_master.json file\n";
@@ -45,8 +92,10 @@ static void intializeProject(std::string_view projectName, std::string_view cano
 }
 
 // build_master meson
-static void invokeMeson(const std::vector<std::string>& args)
+static void InvokeMeson(const std::vector<std::string>& args)
 {
+	// Ensure the meson.build script is upto date
+	RegenerateMesonBuildScript();
 	std::copy(args.begin(), args.end(), std::ostream_iterator<std::string>(std::cout, ", "));
 }
 
@@ -78,14 +127,14 @@ int main(int argc, const char* argv[])
 		scInit->add_option("--canonical_name", canonicalName, 
 			"Conanical name of the Project, typically used for naming files")->required();
 		scInit->add_flag("--force", isForce, "Forcefully overwrites the existing build_master.json file, use it carefully");
-		scInit->callback([&]() { intializeProject(projectName, canonicalName, isForce); });
+		scInit->callback([&]() { IntializeProject(projectName, canonicalName, isForce); });
 	}
 	
 	// Meson Sub command
 	{
 		CLI::App* scMeson = app.add_subcommand("meson", "Invokes meson build system (cli), all the arguments passed to this subcommands goes to actual meson command");
 		scMeson->allow_extras();
-		scMeson->callback([&]() { invokeMeson(scMeson->remaining()); });
+		scMeson->callback([&]() { InvokeMeson(scMeson->remaining()); });
 	}
 
 	CLI11_PARSE(app, argc, argv);
