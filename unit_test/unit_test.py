0323439f77629709f2613d428666def6fcdaf6c0
ravi688 <rp0412204@gmail.com>
2025-01-30 02:16:00 +0530

    Added unit tests for init sub-command  - The tests check for existing of build_master.json, source/main.cpp and include dir after running the init command with its possible arguments.

--- unit_test/unit_test.py
+++ unit_test/unit_test.py
@@ -7,41 +7,84 @@ import sys
 # For regular expression Matching
 import re
 import logging
+import tempfile
+import arttest
+import os
 
-class ExecutableTest(unittest.TestCase):
+class TestBase(arttest.ArtTest):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Path to your executable - modify as needed
-        self.executable = "build_master"
-
-    def run_with_args(self, args):
-        """
-        Run the executable with given input and return its output
-        """
-        process = subprocess.Popen(
-            [self.executable] + args,
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True
-        )
-
-        stdout, stderr = process.communicate()
-        return stdout.split('\n'), stderr.split('\n') if len(stderr) > 0 else None, process.returncode
+        super().__init__('build_master', *args, **kwargs)
+        return
 
     def assert_string_matches_regex(self, string_data, regex_pattern):
         match = re.search(regex_pattern, string_data)
         self.assertIsNotNone(match, f'String \'{string_data}\' doesn\'t match the regex \'{regex_pattern}\'')
+        return
+    
+    def tearDown(self):
+        self.cleanup()
+        return
+
+class TestVersion(TestBase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        return
 
     def test_version(self):
-        stdout, stderr, returncode = self.run_with_args(["--version"])
-        self.assertEqual(returncode, 0)  # Check program exited normally
+        output = self.run_with_args(["--version"])
+        self.assertEqual(output.returncode, 0)  # Check program exited normally
         # build_mater --version
         # Build Master 1.0.0
         # Build Type: Debug
-        self.assert_string_matches_regex(stdout[0], r'^Build Master \d+\.\d+\.\d+$')
-        self.assert_string_matches_regex(stdout[1], r'^Build Type: (Debug|Release)$')
-        self.assertIsNone(stderr)
+        self.assert_string_matches_regex(output.stdout[0], r'^Build Master \d+\.\d+\.\d+$')
+        self.assert_string_matches_regex(output.stdout[1], r'^Build Type: (Debug|Release)$')
+        self.assertIsNone(output.stderr)
+        output.cleanup()
+        return
+
+    def run_test_init(self, is_cpp = False):
+        output = self.run_with_args(['init', '--name=MyProject', '--canonical_name=myproject'] + (['--create-cpp'] if is_cpp else []))
+        self.assertEqual(output.returncode, 0)
+        self.assertIsNone(output.stderr)
+
+        output.assert_exists_file('build_master.json')
+        output.assert_exists_dir('source')
+        output.assert_exists_dir('include')
+        output.assert_exists_file('source/main.cpp' if is_cpp else 'source/main.c')
+        output.cleanup()
+        return
+
+    # build_master init --name=MyProject --canonical_name=myproject
+    def test_init(self):
+        self.run_test_init()
+        return
+
+    # build_master init --name=MyProject --canonical_name=myproject --create-cpp
+    def test_int_cpp(self):
+        self.run_test_init(True)
+        return
+
+    def run_test_init_directory(self, is_cpp = False):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            output = self.run_with_args(['init', '--name=MyProject', '--canonical_name=myproject', f'--directory={temp_dir}'] + ['--create-cpp'] if is_cpp else [])
+            self.assertEqual(output.returncode, 0)
+            self.assertIsNone(output.stderr)
+            output.assert_exists_file(os.path.join(temp_dir, 'build_master.json'))
+            output.assert_exists_dir(os.path.join(temp_dir, 'source'))
+            output.assert_exists_dir(os.path.join(temp_dir, 'include'))
+            output.assert_exists_file(os.path.join(temp_dir, 'source/main.cpp' if is_cpp else 'source/main.c'))
+            output.cleanup()
+        return
+
+    # build_master init --name=MyProject --canonical_name=myproject --directory=<some temp dir>
+    def test_init_directory(self):
+        self.run_test_init_directory()
+        return
+
+    # build_master init --name=MyProject --canonical_namoe=myproject --directory=<some temp dir> --create-cpp
+    def test_init_directory(self):
+        self.run_test_init_directory(True)
+        return
 
 if __name__ == '__main__':
     unittest.main()
