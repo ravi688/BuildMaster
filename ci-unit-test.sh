a6d26bf480d7031bee6ed0fea9cb0a3933d3b7c1
ravi688 <rp0412204@gmail.com>
2025-03-14 20:55:01 +0530

    Renamed unit_test.py to preliminary_tests.py; and added more comments

--- ci-unit-test.sh
+++ ci-unit-test.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
 build_master_meson --version
 build_master --version
 build_master --meson-build-template-path
 
-python ./unit_test/unit_test.py -v
+python ./unit_test/preliminary_tests.py -v
