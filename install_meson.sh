a3e931a5f56986b9955206ecb68b0b0de7bd09e8
ravi688 <rp0412204@gmail.com>
2025-03-20 19:46:04 +0530

    Removed meson specific install logic from install_meson.sh  - It only now contains git cloning and executing the install.sh script in the cloned dir (meson root)

--- install_meson.sh
+++ install_meson.sh
@@ -25,32 +25,4 @@ echo "Pulling origin ravi688-meson"
 
 
 # ---------------- Install build_master_meson ----------------------
-
-# Platform detection
-if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "mingw"* ]]; then
-	PLATFORM="MINGW"
-else
-	PLATFORM="LINUX"
-fi
-
-# Make sure pyinstaller is available
-if ! command -v pyinstaller &> /dev/null; then
-	echo "pyinstaller not avaiable, installing it using pip"
-	pip install --break-system-packages pyinstaller
-fi
-
-# Package meson into one executable
-(cd $CLONE_PATH && pyinstaller --onefile --runtime-hook=runtime_hook.py --add-data "mesonbuild:mesonbuild" meson.py)
-
-if [ -z $INSTALL_PREFIX ]; then
-	INSTALL_PREFIX="/usr"
-fi
-
-# Copy the executable to the install directory
-if [[ "$PLATFORM" == "MINGW" ]]; then
-    echo "Copying dist/meson.exe to ${INSTALL_PREFIX}/bin/build_master_meson.exe"
-    (cd $CLONE_PATH && cp dist/meson.exe "${INSTALL_PREFIX}/bin/build_master_meson.exe")
-else
-    echo "Copying dist/meson to ${INSTALL_PREFIX}/bin/build_master_meson"
-    (cd $CLONE_PATH && cp dist/meson "${INSTALL_PREFIX}/bin/build_master_meson")
-fi
+(cd $CLONE_PATH && ./install.sh)
