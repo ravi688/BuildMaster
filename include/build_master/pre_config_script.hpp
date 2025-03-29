455c0817247e2adc5ce9f69e8423caa7db434aa0
ravi688 <rp0412204@gmail.com>
2025-03-30 02:06:20 +0530

    Added pkg_config_root_hook key in build_master.json for pre-config hook with root privileges

--- include/build_master/pre_config_script.hpp
+++ include/build_master/pre_config_script.hpp
@@ -2,4 +2,5 @@
 
 #include <string_view>
 
+// Returns true if either of 'pre_config_hook' or 'pre_config_root_hook' is given
 bool RunPreConfigScript(std::string_view directory);
--- source/pre_config_script.cpp
+++ source/pre_config_script.cpp
@@ -7,20 +7,40 @@
 
 static constexpr std::string_view gBash = "bash";
 
+static std::optional<bool> RunPreConfigScript(const json& buildMasterJson, std::string_view directory, std::string_view hookName, std::string_view logMsg, bool isRoot = false)
+{
+	if(auto result = GetJsonKeyValueOrNull<std::string>(buildMasterJson, hookName); result.has_value())
+	{
+		spdlog::info(logMsg);
+		auto returnCode = invoke::Exec({ std::string { gBash }, result.value() }, directory, isRoot);
+		return { returnCode == 0 };
+	}
+	return { };
+}
+
 bool RunPreConfigScript(std::string_view directory)
 {
 	json buildMasterJson = ParseBuildMasterJson(directory);
-	if(auto result = GetJsonKeyValueOrNull<std::string>(buildMasterJson, "pre_config_hook"); result.has_value())
+
+	// Run pre_config_hook	
+	auto result1 = RunPreConfigScript(buildMasterJson, directory, "pre_config_hook", "Running pre-config hook script");
+	if(result1)
 	{
-		spdlog::info("Running pre-config hook script");
-		auto returnCode = invoke::Exec({ std::string { gBash }, result.value() }, directory);
-		if(returnCode < 0)
-		{			
-			spdlog::error("pre-config hook script returned non-zero code");
-			exit(returnCode);
-		}
-		spdlog::info("pre-config hook script run success");
-		return true;
+		if(result1.value())
+			spdlog::info("pre-config hook script run success");
+		else
+			spdlog::info("pre-config hook script returned non-zero code");
 	}
-	return false;
+
+	// Run pre_config_root_hook
+	auto result2 = RunPreConfigScript(buildMasterJson, directory, "pre_config_root_hook", "Running pre-config hook script with root privileges", true);
+	if(result2)
+	{
+		if(result2.value())
+			spdlog::info("pre-config hook script with root privileges run success");
+		else
+			spdlog::info("pre-config hook script with root privileges returned non-zero code");
+	}
+
+	return result1 || result2;
 }
