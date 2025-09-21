ba39ab65a9d03089d82c42520ae7e5061d86feff
Ravi Prakash Singh <rp0412204@gmail.com>
2025-09-21 22:59:17 +0530

    Add support for building and installing inside a docker container (#99)
    * check if the scripts are running inside docker container and set NO_ROOT to empty
    
    * Check for if  is available or not
    
    * Do not exit if dropping root privileges fails, consider it as an not an error
    
    * fixup bash scripts
    
    * fixup shell scripts

--- install_build_master.sh
+++ install_build_master.sh
@@ -5,6 +5,21 @@ if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "mingw"* ]]; 
         PLATFORM="MINGW"
 else
         PLATFORM="LINUX"
+        if [ "$EUID" -ne 0 ]; then
+                echo "This script must be run as root. Please use sudo."
+                exit -1
+        fi
+
+        is_in_docker() {
+                [ -f "/.dockerenv" ] || grep -qa docker /proc/1/cgroup
+        }
+
+        # Detect if we are running on docker container
+        if is_in_docker || [ -z "${SUDO_USER-}"]; then
+                NO_ROOT=""
+        else
+                NO_ROOT="sudo -u $SUDO_USER"
+        fi
 fi
 
 if [ -n "${DEBUG}" ]; then
@@ -19,8 +34,8 @@ if ./install_meson.sh; then
 		build_master_meson compile -C build && \
 		build_master_meson install -C build --skip-subprojects)
 	else
-		(sudo -u $SUDO_USER build_master_meson setup build --reconfigure --buildtype=$BUILD_TYPE && \
-		sudo -u $SUDO_USER build_master_meson compile -C build && \
+		($NO_ROOT build_master_meson setup build --reconfigure --buildtype=$BUILD_TYPE && \
+		$NO_ROOT build_master_meson compile -C build && \
 		build_master_meson install -C build --skip-subprojects)
 	fi
 fi
