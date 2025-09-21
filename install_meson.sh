87bd3850457b06cc6b23809f291de5eb936b455d
ravi688 <rp0412204@gmail.com>
2025-09-21 08:26:29 -0800

    check if the scripts are running inside docker container and set NO_ROOT to empty

--- install_meson.sh
+++ install_meson.sh
@@ -2,12 +2,29 @@
 
 # example: CLONE_DIR=<my clone path> INSTALL_PREFIX=/usr ./install_meson.sh
 
+# Stop executing the script if any command fails
+set -e
 
 # Platform detection
 if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "mingw"* ]]; then
         PLATFORM="MINGW"
 else
         PLATFORM="LINUX"
+	if [ "$EUID" -ne 0 ]; then
+		echo "This script must be run as root. Please use sudo."
+		exit -1
+	fi
+
+	is_in_docker() {
+    		[ -f "/.dockerenv" ] || grep -qa docker /proc/1/cgroup
+	}
+
+	# Detect if we are running on docker container
+	if is_in_docker; then
+		NO_ROOT=""
+	else
+		NO_ROOT="sudo -u $SUDO_USER"
+	fi
 fi
 
 # --------------- Git repository cloning --------------------------
@@ -26,7 +43,7 @@ else
 	if [[ "$PLATFORM" == "MINGW" ]]; then
 		git clone $GIT_REPO_PATH $CLONE_PATH
 	else
-		sudo -u $SUDO_USER git clone $GIT_REPO_PATH $CLONE_PATH
+		$NO_ROOT git clone $GIT_REPO_PATH $CLONE_PATH
 	fi
 fi
 
@@ -38,9 +55,9 @@ if [[ "$PLATFORM" == "MINGW" ]]; then
 			&& git checkout ravi688-meson \
 			&& git pull origin ravi688-meson --ff)
 else
-	(cd $CLONE_PATH && sudo -u $SUDO_USER git fetch \
-		&& sudo -u $SUDO_USER git checkout ravi688-meson \
-		&& sudo -u $SUDO_USER git pull origin ravi688-meson --ff)
+	(cd $CLONE_PATH && $NO_ROOT git fetch \
+		&& $NO_ROOT git checkout ravi688-meson \
+		&& $NO_ROOT git pull origin ravi688-meson --ff)
 fi
 
 
