e8f68bf32d258dfaeeb6086f95d62f37b2cdbd1c
Ravi Singh <rp0412204@gmail.com>
2026-04-13 11:40:10 +0530

    Update ci-mingw-setup.sh

--- ci-mingw-setup.sh
+++ ci-mingw-setup.sh
@@ -7,8 +7,7 @@ pacman -Sy --noconfirm mingw-w64-x86_64-meson \
 ./install_meson.sh
 ./ci-build.sh
 
-pacman -S mingw-w64-x86_64-zlib
-pacman -S mingw-w64-x86_64-mbedtls
+pacman -Sy mingw-w64-x86_64-zlib mingw-w64-x86_64-mbedtls
 git clone https://github.com/ravi688/IXWebSocket.git
 cd IXWebSocket
 cmake -S . -B build -GNinja -DCMAKE_INSTALL_PREFIX=/mingw64 -DCMAKE_BUILD_TYPE=Release
