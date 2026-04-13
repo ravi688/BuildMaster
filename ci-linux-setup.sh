25210e20ca858afecdd4ce5e054616e494a3987c
Ravi Singh <rp0412204@gmail.com>
2026-04-13 11:52:06 +0530

    Ci fix netsocket (#115)
    * Update ci-linux-setup.sh
    
    * Update ci-mingw-setup.sh
    
    * Update ci-mingw-setup.sh

--- ci-linux-setup.sh
+++ ci-linux-setup.sh
@@ -7,8 +7,18 @@ apt-get install python3
 apt-get install python-is-python3
 apt-get install libspdlog-dev
 apt-get install pip
+apt-get install cmake
 
 chmod +x ./install_meson.sh
 ./install_meson.sh
 chmod +x ./ci-build.sh
 ./ci-build.sh
+
+
+apt install libssl-dev
+apt install libmbedtls-dev
+git clone https://github.com/ravi688/IXWebSocket.git
+cd IXWebSocket
+cmake -S . -B build -GNinja -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release
+cmake --build build
+cmake --install build
--- ci-mingw-setup.sh
+++ ci-mingw-setup.sh
@@ -6,3 +6,10 @@ pacman -Sy --noconfirm mingw-w64-x86_64-meson \
 
 ./install_meson.sh
 ./ci-build.sh
+
+pacman -Sy mingw-w64-x86_64-zlib mingw-w64-x86_64-mbedtls
+git clone https://github.com/ravi688/IXWebSocket.git
+cd IXWebSocket
+cmake -S . -B build -GNinja -DCMAKE_INSTALL_PREFIX=/mingw64 -DCMAKE_BUILD_TYPE=Release
+cmake --build build
+cmake --install build
